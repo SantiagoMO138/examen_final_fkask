cf2c0db9214290f46ef731d88e2ecf735b56faf5
SantiagoMO138 <santiago.a.murillo.o@gmail.com>
2025-06-13 19:10:40 -0400

    6546.564

--- models/base.py
+++ models/base.py
@@ -1,3 +1,4 @@
 from sqlalchemy import create_engine
 #engine = create_engine('postgresql://postgres:5d48phTbdpLqb4X@localhost:5432/vaterinaria')
 engine = create_engine('postgresql://veterinaria_lcd_user:IqW8S1BpNJShN7hMkueUI4549QuQZtge@dpg-d16aikmmcj7s73buj8n0-a.oregon-postgres.render.com/veterinaria_lcd')
+
